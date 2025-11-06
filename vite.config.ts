68b957d7bc5dd649f8f5915efa6e507dce1555cb
rhicksrad <rhicks@radindiana.com>
2025-11-05 22:21:24 -0500

    Fix Pages workflow and configure pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
 const isCI = !!process.env.CI;
+
 export default defineConfig({
   base: isCI && repo ? `/${repo}/` : '/',
 });
